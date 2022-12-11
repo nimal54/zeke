8a3cac28427297e7e635e28b5d48c4208dec7b4e
nimal54 <nimalviju54@gmail.com>
2022-12-12 00:18:48 +0530

    footer, header, home

---
+++ src/page/home.page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HomeComponent from "../component/home/home.component"
+
+const HomePage = () =>{
+    return <HomeComponent/>
+
+}
+
+export default HomePage
---
+++ src/utils/system.constant.js
@@ -0,0 +1,12 @@
+
+// color 
+export const PRIMARY_COLOR = '#0B2742'
+export const SECONDARY_COLLOR = '#44EBA7'
+export const COMMON_COLOR = '#FFFFFF'
+
+// font 
+export const DEFAULT_FONT = 'Poppins'
+
+//variables
+export const COMPANY_NAME = 'Zeke International'
+
